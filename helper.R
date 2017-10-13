aa77bbeee55fed0d55624bc360786d285faf20b4
Todd K. Hartman <tkhartman@gmail.com>
2017-10-13 14:53:16 +0100

    Update helper.R

--- helper.R
+++ helper.R
@@ -3,3 +3,8 @@
 maxn <- function(n) function(x) order(x, decreasing = TRUE)[n]  # function
 df$first <- apply(df, 1, function(x)x[maxn(1)(x)])  # Maximum value
 df$second <- apply(df, 1, function(x)x[maxn(1)(x)])  # Second highest value
+                   
+##' [rescale01] Function to rescale a variable from 0 to 1
+rescale01 <- function(x) {
+  (x - min(x)) / ((max(x)) - min(x))
+}
