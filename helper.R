ed9a44ee3fdd02201282e9abc008432148713d86
Todd K. Hartman <tkhartman@gmail.com>
2017-11-09 10:09:24 +0000

    Update helper.R

--- helper.R
+++ helper.R
@@ -1,5 +1,5 @@
 
-##' Find nth highest column position for each row
+##' [maxn] Find nth highest column position for each row
 maxn <- function(n) function(x) order(x, decreasing = TRUE)[n]  # function
 df$first <- apply(df, 1, function(x)x[maxn(1)(x)])  # Maximum value
 df$second <- apply(df, 1, function(x)x[maxn(1)(x)])  # Second highest value
@@ -7,4 +7,19 @@ df$second <- apply(df, 1, function(x)x[maxn(1)(x)])  # Second highest value
 ##' [rescale01] Function to rescale a variable from 0 to 1
 rescale01 <- function(x) {
   (x - min(x)) / ((max(x)) - min(x))
+}
+                   
+##' [clean.text] Function to clean Twitter data
+clean.text <- function(x)
+{
+    x <- tolower(x)      # To lower case
+    x <- gsub("rt", "", x)  # Remove 'rt'
+    x <- gsub("@\\w+", "", x)  # Remove '@'
+    x <- gsub("[[:punct:]]", "", x)  # Remove punctuation
+    x <- gsub("[[:digit:]]", "", x)  # Remove numbers
+    x <- gsub("http\\w+", "", x)  # Remove 'http' links
+    x <- gsub("[ |\t]{2,}", "", x)  # Remove tabs
+    x <- gsub("^ ", "", x)  # Remove leading blank spaces
+    x <- gsub(" $", "", x)  # Remove ending blank spaces
+    return(x)
 }
